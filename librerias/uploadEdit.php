80f30649aa092f3e5b6c98a15cf72e6c1a2ff988
RonaL <ronald4261@hotmail.com>
2013-07-08 18:05:22 -0500

    eliminando imagenes innecesarias

--- librerias/uploadEdit.php
+++ librerias/uploadEdit.php
@@ -1,124 +1,120 @@
 <?php
 
 date_default_timezone_set('America/Lima');
-	session_start();
+
+    session_start();
+
+        if(isset($_SESSION['tmp']['ruta_pdf_temporal'])){
+            @unlink($_SESSION['tmp']['ruta_pdf_temporal']);
+            unset($_SESSION['tmp']['ruta_pdf_temporal']);
+        }
+
 $tipo = substr($_FILES['fileUpload']['type'], -3);
 
 //  Definimos Directorio donde se guarda el archivo
 $dir = '../tmpUpload/';
-
-
 //$codPublicacionesEdit=$_POST['codPublicaciones'];
-//$date_in=$_POST['date_in'];
+$date_in=$_POST['date_in'];
 
-$tipoResolucion=$_POST['tipoResolucion'];
+$ano=$_POST['ano'];
 
-$tipoResolucion_description="";
-switch ($tipoResolucion){
-    case 1:
-        $tipoResolucion_description="respre";
-    break;        
-    case 2:
-        $tipoResolucion_description="resdir";
-    break;
-    case 3:
-        $tipoResolucion_description="resjef";
-    break;        
-    case 4:
-        $tipoResolucion_description="directivas";
-    break;        
-    case 5:
-        $tipoResolucion_description="contratos_logistica";
-    break;        
-    case 6:
-        $tipoResolucion_description="contratos_rrhh";
-    break;        
-    case 7:
-        $tipoResolucion_description="adRRHH";
-    break;        
-    case 8:
-        $tipoResolucion_description="adLOG";
-    break;        
+//if($_SESSION["tipoDocumento"]!=4){
+    $tipoDocumento=str_replace("_","",$_SESSION["tipoDocumento"]);
+    $subcategory=str_replace("_","",$_SESSION["subcategory"]);
+/*}
+else{
+    $tipoDocumento="ponencia";
+    $subcategory="ponencia";
 
 }
-
-$year=$_POST['year'];
-
-
-/*Codigo*/
-$correlativo=$_POST['correlativoUp'];
-
-    $Icero="0";
-    $IIceros="00";
-//    $IIIceros="000";
-//    $IVceros="0000";
-//    $Vceros="00000";
-
-if($correlativo<=9)
-        $correlativo="$IIceros$correlativo";
-
-elseif($correlativo<=99)
-        $correlativo="$Icero$correlativo";
-
-/*
-elseif($correlativo<=999)
-        $correlativo="$Iceros$correlativo";
 */
+    
+$codArea=isset($_SESSION["idarea"])?$_SESSION["idarea"]:"";
 
-/*
-       $archivoUpload="IGP_".$tipoResolucion_description."_".$correlativo."-".$year.".pdf";
-       $destino="/datos/resoluciones/".$tipoResolucion_description."/".$year."/".$archivoUpload;
-*/
-       $archivoUpload=$tipoResolucion_description."".$correlativo."_".$year.".pdf";
+$idtypedocument=isset($_SESSION["idtypedocument"])?$_SESSION["idtypedocument"]:0;
+$idsubcategory=isset($_SESSION["idsubcategory"])?$_SESSION["idsubcategory"]:0;
+
+$nro=1;
+$fecha=date("Y-m-d");
+$hora=date("H:i:s");
+$strtotime=strtotime ("now");
+    $codigo="IGP-".$codArea."-".$idtypedocument."-".$idsubcategory."-".$strtotime;
+//$archivoUpload=$fecha."_".$hora."_".$codigo.".pdf";
+$archivoUpload=$codigo.".pdf";
+//$archivoPortada=$fecha."_".$hora."_".$codigo;
+
+
        $destino=$dir.$archivoUpload;
 
 
 //  Intentamos Subir Archivo
 //  (1) Comprovamos que existe el nombre temporal del archivo
-if (isset($_FILES['fileUpload']['tmp_name'])) {
-
-	    //  (2) - Comprovamos que se trata de un archivo de imágen
-	 if ($tipo == 'pdf') {
-		//  (3) Por ultimo se intenta copiar el archivo al servidor.
-		if (!copy($_FILES['fileUpload']['tmp_name'],$destino)){
-			    echo '<script> alert("Error al Subir el Archivo");</script>';
-		    }
-		else{
-			//El archivo pdf se ha copiado con Exito.
-                        echo '<script>parent.resultadoUpload(0, "'.$archivoUpload.'");</script>';
+if ($_FILES['fileUpload']['tmp_name']!="") {
+    
+        //  (2) - Comprobamos de que archivo se trata 
+     if ($tipo == 'jpg' or $tipo == 'png') {
+             //echo '<script>alert("Hola")</script>';
+             //echo '<script>parent.efectoUpload();</script>';
+        //  (3) Por ultimo se intenta copiar el archivo al servidor.
+        if (!copy($_FILES['fileUpload']['tmp_name'],$destino)){
+                echo '<script> alert("Error al Subir el Archivo");</script>';
+            }
+        else{
                     
-		    
-		    //$queryBDa= "update ofertas set pdf='$destino' where tipo='CAS'";
-		    //@mysql_query($queryBDa)or die("Invalid Query: $queryBDa <br>" . mysql_error());
-		}
-	}
-	 else{
-		//El Archivo que se intenta subir NO ES del tipo PDF.
-		echo '<script>parent.resultadoUpload(2, "'.$archivoUpload.'");</script>';
+/******Convierte la primera pagina del pdf en una imágen png*****/
+//exec("convert ".$destino."[0] ".$dir.$archivoPortada.".png");
+/****************************************************************/
+                    
+                    
+                    if(is_uploaded_file($_FILES['fileUpload']['tmp_name'])){
+                        sleep(3);
+                        echo '<script>parent.resultadoUpload(0, "'.$archivoUpload.'");</script>';        
+                        //echo '<script>parent.efectoUpload2();</script>';
+                        //echo '<script>alert("Hola")</script>';
+                    }
+                            //echo '<script>parent.document.getElementById("boton_guardar").innerHTML="<font color=green>Se terminó de subir</font>";</script>';
+
+        }
+    }
+     else{
+        //El Archivo que se intenta subir NO ES del tipo PDF.       
+                echo '<script>parent.resultadoUpload(2, "'.$archivoUpload.'");</script>';
+                
          }
 }
-
-else echo '<script> alert("El Archivo no ha llegado al Servidor.");</script>';
-
+else {
+    //echo '<script> alert("El Archivo no ha llegado al Servidor.");</script>';
+    echo '<script>parent.resultadoUpload(1, "'.$archivoUpload.'");</script>';
+}
 
 echo '<p>Nombre Temporal: '.$_FILES['fileUpload']['tmp_name'].'</p>';
-echo '<p>Nombre en el Server: '.$_FILES['fileUpload']['name'].'</p>';
+echo '<p>Nombre en el Server: '.$archivoUpload;
 echo '<p>Tipo de Archivo: '.$_FILES['fileUpload']['type'];
 
-//echo '<p>Codigo: '.$_POST['codPublicaciones'];
-//echo '<p>Date: '.$date_in;
-echo '<p>Tipo Documento: '.$_POST['tipoResolucion'];
-//echo '<p>Tipo Publicacion: '.$_POST['tipoPublicacion'];
+if(isset($_SESSION["edit"])){
+    $_SESSION["edit"]["pdf_upload"]=$archivoUpload;
+
+    $_SESSION["edit"]["ruta_pdf_temporal"]="tmpUpload/".$archivoUpload;
+}
+elseif(isset($_SESSION["tmp"])){
+    $_SESSION["tmp"]["pdf"]=$archivoUpload;
+
+    $_SESSION["tmp"]["ruta_pdf_temporal"]="tmpUpload/".$archivoUpload;
+}
+/*
+echo '<p>Codigo: '.$_POST['codPublicaciones'];
+echo '<p>Date: '.$date_in;
+echo '<p>Tipo Documento: '.$_POST['tipoDocumento'];
+echo '<p>Tipo Publicacion: '.$_POST['tipoPublicacion'];
+*/
 //echo '<p>'.$queryBDa;
 echo '<p>'.$destino;
-
-$_SESSION["ruta_pdf_temporal"]="tmpUpload/".$archivoUpload;
-$_SESSION["pdf_edit"]=$archivoUpload;
-
 echo '<br>';
 echo '<p>';
 print_r($_SESSION);
 
 echo '<p>';
 
+
+//createJpg($archivoUpload);
 ?>
